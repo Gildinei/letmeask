46079d38253b340e81c537061b35736f9d05b228
Gildinei S. Anunciação <gildineisa@gmail.com>
2021-06-24 16:49:27 -0300

    Feat: adicionar funcionalidades de navegação entre rotas

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import { Button } from '../components/Button';
 
 import illustrationImg from '../assets/images/illustration.svg';
@@ -28,7 +30,7 @@ export function NewRoom() {
                         </Button>
                     </form>
                     <p>
-                        Quer entrar em uma sala existente? <a href="#">Clique aqui</a>
+                        Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
                     </p>
                 </div>
             </main>
