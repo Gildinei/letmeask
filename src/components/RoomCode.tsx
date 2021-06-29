b4953db1d217637482675e462a02f036dcd9b867
Gildinei Santos Anunciação <gildineisa@gmail.com>
2021-06-29 15:09:55 -0300

    Feat: adiciona sala de perguntas

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -2,14 +2,22 @@ import copyImg from '../assets/images/copy.svg';
 
 import '../styles/room-code.scss';
 
-export function RoomCode() {
+type RoomCodeProps = {
+    code: string;
+};
+
+export function RoomCode(props: RoomCodeProps) {
+    function copyRoomCodeToClipboard() {
+        navigator.clipboard.writeText(props.code);
+    };
+
     return (
         <button className="room-code">
             <div>
                 <img src={copyImg} alt="Copy room code" />
             </div>
-            
-            <span>Sala #-Md39FSdBc-wKZzJv8w3</span>
+
+            <span>Sala #{props.code}</span>
         </button>
     );
 };
