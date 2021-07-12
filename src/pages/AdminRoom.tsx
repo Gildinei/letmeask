5f0b92259135266f40d28a6e00bb05a31ed31be2
Gildinei S. Anunciação <gildineisa@gmail.com>
2021-07-12 10:32:26 -0300

    Feat: adiciona funcionalide de likes

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,8 +1,8 @@
-import { FormEvent, useState } from 'react';
+// import { FormEvent, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { database } from '../services/firebase';
-import { useAuth } from '../hooks/useAuth';
+// import { database } from '../services/firebase';
+// import { useAuth } from '../hooks/useAuth';
 
 import logoImg from '../assets/images/logo.svg';
 
@@ -18,38 +18,11 @@ type RoomParams = {
 };
 
 export function AdminRoom() {
-    const { user } = useAuth();
+    // const { user } = useAuth();
     const params = useParams<RoomParams>();
-    const [newQuestion, setNewsQuestion] = useState('');
     const roomId = params.id;
 
     const { title, questions } = useRoom(roomId);
-
-    async function handleSendQuestion(event: FormEvent) {
-        event.preventDefault();
-
-        if (newQuestion.trim() === '') {
-            return;
-        };
-
-        if (!user) {
-            throw new Error("You must be logged in");
-        };
-
-        const question = {
-            content: newQuestion,
-            author: {
-                name: user.name,
-                avatar: user.avatar,
-            },
-            isHighlighted: false,
-            isAnswered: false,
-        };
-
-        await database.ref(`rooms/${roomId}/questions`).push(question);
-
-        setNewsQuestion('');
-    };
 
     return (
         <div id="page-room">
